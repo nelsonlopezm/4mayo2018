483f0e4c688b62d9f76010cd932aad76090cfb7b
Sala K406 <salak406@itmsalas.edu.co>
2018-05-04 07:10:38 -0500

    actualizando

--- 4mayo.sql
+++ 4mayo.sql
@@ -1,3 +1,53 @@
+
+CREATE TABLE  test_case (
+    col_pk  integer          NOT NULL,
+    col_i1  integer          NOT NULL,
+    col_d1  date             NOT NULL,
+    col_c1  VARCHAR2 (100)   NOT NULL,
+    col_i2  integer          NOT NULL,
+    col_d2  date             NOT NULL,
+    col_c2  VARCHAR2 (100)   NOT NULL
+    );
+    
+    
+    create or replace FUNCTION repeat (c char, n integer) return varchar
+    is
+    s varchar2 (100):= '';
+    begin
+      for i in 1 .. n loop
+        s := s || c;
+        end loop;
+        RETURN (s);
+    end;
+    
+    ---- procedimiento anonimo
+    
+    declare
+    row_count integer := 1000000;
+    BEGIN
+      for i in 1 .. row_count loop
+        insert into test_case
+          values (i, mod(i,100),sysdate-mod(i,100),
+            repeat (substr ('ABCDEFGHIJ',mod(i,10),1),10),
+            mod(1,100), SYSDATE-MOD(i,1000),
+            repeat (substr ('ABCDEFGHIJ',mod(i,10),1),10)
+      );
+    
+        if (mod (i,100) = 0) then
+          commit;
+        end if;
+      end loop;
+    end;
+    
+        
+        
+
+
+
+
+
+
+
 CREATE TABLE  employees (
     employee_id NUMBER          NOT NULL,
     first_name VARCHAR2 (1000)  NOT NULL,
@@ -17,3 +67,5 @@ SELECT  first_name, last_name
     
     
     ---------
+    
+    select * from SYS.PLAN_TABLE$
